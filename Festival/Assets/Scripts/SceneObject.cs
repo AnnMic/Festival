76f8ff41972ec0368e632dbc2e1ba0244332c167
Ann Michélsen <ann@maginteractive.com>
2015-04-09 14:25:44 +0200

    list for scene objects

---
+++ Festival/Assets/Scripts/SceneObject.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SceneObject : MonoBehaviour {
+
+
+	public List<GameObject> allObjects = new List<GameObject>();
+
+	public List<GameObject> hungerObjects = new List<GameObject>();
+	public List<GameObject> toiletObjects = new List<GameObject>();
+	public List<GameObject> thirstObjects = new List<GameObject>();
+	public List<GameObject> hygieneObjects = new List<GameObject>();
+	public List<GameObject> funObjects = new List<GameObject>();
+
+}
