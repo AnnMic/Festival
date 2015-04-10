1607e5b9a38567ca871c31f7a312467b33613ec6
Oleg Fischer <oleg@maginteractive.se>
2015-04-10 13:39:18 +0200

    Performer billboards added

---
+++ Festival/Assets/Scripts/StarsInConcertController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StarsInConcertController : MonoBehaviour {
+
+	public float switchDelay;
+
+	private int currentPerformer = 0;
+	private GameObject[] starsInConcert;
+
+	void Awake(){
+		starsInConcert = new GameObject[transform.childCount];
+		for (int i=0; i<transform.childCount; i++){
+			starsInConcert[i] = transform.GetChild(i).gameObject;
+		}
+		currentPerformer = Random.Range(0, starsInConcert.Length);
+	}
+
+	void Start(){
+		starsInConcert[currentPerformer].SetActive(true);
+		StartCoroutine(SwitchPerformer(switchDelay));
+	}
+
+	private IEnumerator SwitchPerformer(float delay){
+		yield return new WaitForSeconds(delay);
+		starsInConcert[currentPerformer].SetActive(false);
+		currentPerformer = Random.Range(0, starsInConcert.Length);
+		starsInConcert[currentPerformer].SetActive(true);
+		StartCoroutine(SwitchPerformer(switchDelay));
+	}
+}
